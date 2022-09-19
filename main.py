8f5b5a94bfab15016a10fb0fcdd303183e7f795f
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-09-19 12:54:20 +0200

    aaaa

--- main.py
+++ main.py
@@ -6,6 +6,72 @@
 # default parameter
 # LABBA
 
+# REGEL:_ När vi skriver en funktion sp
+# är dom enda variablerna vi använder = parametrar + lokala
+def addAccount(allAccounts):
+    kontonummer = input("Ange kontonummer")
+    allAccounts[kontonummer] = 0
+
+def addPlayer(allPlayers):
+    namn = input("Ange namn")
+    allPlayers.append(namn)
+
+allAccounts =  {}
+allPlayers =  []
+addAccount(allAccounts)
+addPlayer(allPlayers)
+
+def IsMyndig(age):
+    if age >= 18:
+        return True
+    else:
+        return False
+
+age = int(input("Din ålder"))    
+myndig = IsMyndig(age)
+if myndig:
+    print("Myndig")
+else:
+    print("Icke myndig")
+
+
+
+while True:
+    age = int(input("Ange ålder:"))
+    location = input("Ange plats K/S:")
+    promille = float(input("Ange din promillehalt:"))
+
+
+
+
+
+
+def Hello():
+    age = age + 1
+age = 12
+Hello()
+print(age)
+
+def HittaLangstaOrdet(lista):
+    longestSoFar = lista[0]
+    for one in lista:
+        pass
+        #if len(lon)
+    return longest
+
+lista = ["aaa", "bbb", "Stefan"]
+longest = HittaLangstaOrdet(lista)
+
+
+def Plussa(s1,s2):
+    result = s1 + s2
+    return result
+
+t = "Morgon"
+res = Plussa("God ", t)
+
+
+
 
 
 print("Hej hej")
@@ -37,14 +103,14 @@ def PrintMeny(languageCode):
 def theCalculation(a,b,c,d,e,f=2022,g=1,h=2,lang="SV"):
     pass
 
-theCalculation(12,34,667,134556,7)
+theCalculation(12,34,667,134556,7,2021)
 
 print("Programmet börjar")
 lang = input("Vilket språk vill du köra på - SV/EN?")
 while True:
     PrintMeny(lang)
     #sel = GetIntMenuInput(prompt="Ange val:", minValue=1,maxValue=4)
-    sel = GetIntMenuInput("Ange val:", 1,4)
+    sel = GetIntMenuInput("Ange val:", 1,maxValue=4)
     if sel == 4:
         lang = input("Vilket språk vill du köra på - SV/EN?")
     if sel == 1:
